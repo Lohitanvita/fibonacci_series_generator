b55cd7ee9d61d4f0be4cbd9f7f7285ef1e6e4d5b
Lohitanvita Rompicharla <rlohitanvita@gmail.com>
2023-08-02 13:02:54 -0400

    Created a model "Fibnum" in DB SQLite3 relational database

--- fibonacci_generator/models.py
+++ fibonacci_generator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class FibNum(models.Model):
+    n = models.IntegerField(default=0)
+    value = models.IntegerField(default=0)
+
+    def __int__(self):
+        return (self.n, self.value)
