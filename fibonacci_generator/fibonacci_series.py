350d2ee2daf44c271c2997dd6f37e92a92051020
Lohitanvita Rompicharla <rlohitanvita@gmail.com>
2023-08-02 13:27:03 -0400

    1. made migrations and connected to db sqlite3 database 2. created "fibonacci_series" python script to generate a series of n fibonacci numbers 3. designed 2 html pages for user input and result ouput

---
+++ fibonacci_generator/fibonacci_series.py
@@ -0,0 +1,57 @@
+# Python program to display the Fibonacci sequence
+
+def fibonacci_series(n, n1, n2):
+    results = []
+    if n1 == 0 and n2 == 1:
+        n1, n2 = n1, n2
+    else:
+        n1 = n1 + n2
+        n2 = n1 + n2
+    count = 0
+
+    # check if the number of terms is valid
+    if n <= 0:
+       return "Please enter a positive integer"
+    # if there is only one term, return n1
+    elif n == 1:
+       results.append(n1)
+    # generate fibonacci sequence
+    else:
+       while count < n:
+           results.append(n1)
+           nth = n1 + n2
+           # update values
+           n1 = n2
+           n2 = nth
+           count += 1
+    return results
+
+print(fibonacci_series(5,2,3))
+
+
+"""def fib_series(n,):
+    results = []
+    def recur_fibo(n):
+       if n <= 1:
+           return n
+       else:
+           return(recur_fibo(n-1) + recur_fibo(n-2))
+
+    # check if the number of terms is valid
+    if n <= 0:
+       return ("Plese enter a positive integer")
+    else:
+       # print("Fibonacci sequence:")
+       for i in range(n):
+           results.append((recur_fibo(i)))
+    return results
+"""
+
+# print(fib_series(10))
+# result = recur_fibo(i)
+# return result
+
+
+
+
+
